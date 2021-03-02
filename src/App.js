8cadf2b1e5ba16220520fa859ea5c547fefdcae0
Grecia Delgado <greciadm2596@gmail.com>
2021-03-02 14:28:04 -0500

    Example of ow to use redirect done

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Switch, Redirect } from "react-router-dom";
 
 const Home = () => <h1>Home Page</h1>;
 const Login = () => <h1>Login Page</h1>;
 const SignUp = () => <h1>SignUp Page</h1>;
-const Profile = ({ name }) => <h1>{`Hello ${name}`}</h1>;
+const Profile = ({ name }) => {
+  if (name === "deyvi") {
+    return <Redirect to="/login" />;
+  }
+  return <h1>Profile Page for {name}</h1>;
+};
 
 const Header = () => {
   return (
     <nav className="nav-bar">
-     <Link to="/">Home</Link>
+      <Link to="/">Home</Link>
       <Link to="/login">Login</Link>
       <Link to="/sign-up">SignUp</Link>
-      <Link to="/profile/codeable">Profile</Link>
+      <Link to="/profile/diego">Profile Diego</Link>
+      <Link to="/profile/wences">Profile Wences</Link>
+      <Link to="/non-existing-page">No existing page</Link>
     </nav>
   );
 };
@@ -23,13 +30,16 @@ function App() {
     <Router>
       <div>
         <Header />
-        <Route path="/" exact component={Home} />
-        <Route path="/login" component={Login} />
-        <Route path="/sign-up" component={SignUp} />
-        <Route
-          path="/profile/:names?"
-          render={({ match }) => <Profile name={match.params.names} />}
-        />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/login" component={Login} />
+          <Route path="/sign-up" component={SignUp} />
+          <Route
+            path="/profile/:name"
+            render={({ match }) => <Profile name={match.params.name} />}
+          />
+          <Route render={() => <h1>Page not found</h1>} />
+        </Switch>
       </div>
     </Router>
   );
