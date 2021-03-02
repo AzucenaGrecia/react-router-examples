10d4c820dd025a315ee7f5c8588674fbf269be41
Grecia Delgado <greciadm2596@gmail.com>
2021-03-02 12:24:57 -0500

    exmaple of use link is done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+const Home = () => <h1>Home Page</h1>;
+const Login = () => <h1>Login Page</h1>;
+const SignUp = () => <h1>SignUp Page</h1>;
+const Profile = () => <h1>Profile Page</h1>;
+
+const Header = () => {
+  return (
+    <nav className="navBar">
+      <Link to="/">Home</Link>
+      <Link to="/login">Login</Link>
+      <Link to="/sign-up">SignUp</Link>
+      <Link to="/profile">Profile</Link>
+    </nav>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <Route path="/" exact component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/sign-up" component={SignUp} />
+        <Route path="/profile" component={Profile} />
+      </div>
+    </Router>
   );
 }
 
