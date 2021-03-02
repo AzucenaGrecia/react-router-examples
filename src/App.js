c5c87e2fe16b7b651c5ff303c81f224a881082e4
Grecia Delgado <greciadm2596@gmail.com>
2021-03-02 13:58:54 -0500

    use URL_params to be able to change routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <nav className="nav-bar">
+      <Link to="/profile">Profile</Link>
+      <Link to="/login">Login</Link>
+      <Link to="/logout">Logout</Link>
+    </nav>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <Route
+          path="/:page"
+          render={({ match }) => <h1>Param: {match.params.page}</h1>}
+        />
+      </div>
+    </Router>
   );
 }
 
